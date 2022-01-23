49969bcec8fcc3fd3685a66f35126c2cd8eab1b5
Olaf Conijn <olaf_conijn@hotmail.com>
2022-01-23 20:22:58 +0100

    feat: added error messages for unseupported statement types

--- src/lang-support/transformers/index.ts
+++ src/lang-support/transformers/index.ts
@@ -1,14 +1,17 @@
 import { callStatementTransformer } from "./call-statement";
+import { forOfStatementTransformer } from "./for-of-statement";
 import { ifStatementTransformer } from "./if-statement";
 import { promiseAllStatementTransformer } from "./promise-all-statement";
 import { returnStatementTransformer } from "./return-statement";
 import { switchStatementTransformer } from "./switch-statement";
 import { throwStatementTransformer } from "./throw-statement";
 import { tryStatementTransformer } from "./try-statement";
+import { unsupportedStatementTransformer } from "./unsupported";
 import { variableStatementTransformer } from "./variable-statement";
 import { whileStatementTransformer } from "./while-statement";
 
 export const transformers = [
+  unsupportedStatementTransformer,
   ifStatementTransformer,
   returnStatementTransformer,
   switchStatementTransformer,
@@ -17,5 +20,6 @@ export const transformers = [
   variableStatementTransformer,
   promiseAllStatementTransformer,
   callStatementTransformer,
-  whileStatementTransformer
+  whileStatementTransformer,
+  forOfStatementTransformer
 ];
