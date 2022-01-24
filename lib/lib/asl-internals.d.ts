c8739f09ce3108099efb09b015e453522de0b511
Olaf Conijn <olaf_conijn@hotmail.com>
2022-01-24 23:26:44 +0100

    fix: remove lib from source

--- lib/lib/asl-internals.d.ts
+++
@@ -1,3 +0,0 @@
-import { Operator } from "asl-types/dist/choice";
-export declare const internalWaitSeconds: (seconds: number) => Promise<void>;
-export declare const internalEvaluateOperator: (operator: Operator) => boolean;
