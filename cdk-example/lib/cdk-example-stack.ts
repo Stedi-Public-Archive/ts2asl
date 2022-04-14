af1331cece522583c0005e9b855b18597ff25e09
Olaf Conijn <olaf_conijn@hotmail.com>
2022-04-14 21:29:07 +0200

    fix: update cdk example project

--- cdk-example/lib/cdk-example-stack.ts
+++ cdk-example/lib/cdk-example-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as iam from '@aws-cdk/aws-iam';
+import * as logs from '@aws-cdk/aws-logs';
 import * as ts2asl from '@ts2asl/cdk-typescript-statemachine';
+import { LogLevel } from '@aws-cdk/aws-stepfunctions';
 
 export class CdkExampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -10,16 +12,21 @@ export class CdkExampleStack extends cdk.Stack {
     const executionRole = new iam.Role(this, "executionRole", {
       assumedBy: new iam.ServicePrincipal("states.amazonaws.com")
     })
-
     executionRole.addManagedPolicy(iam.ManagedPolicy.fromManagedPolicyArn(this, "executionRolePolicy", "arn:aws:iam::aws:policy/service-role/AWSLambdaRole"))
+
+    const logGroup = new logs.LogGroup(this, "TypescriptStateMachineLogs", {
+      logGroupName: "/aws/vendedlogs/states/typescript-hello-world",
+      retention: logs.RetentionDays.ONE_MONTH,
+    });
 
     // example resource
     new ts2asl.TypescriptStateMachine(this, "TypescriptStateMachine", {
       programName: "hello-world",
       defaultFunctionProps: {},
       defaultStepFunctionProps: {
-        stateMachineType: "EXPRESS",
-        roleArn: executionRole.roleArn
+        role: executionRole,
+        tracingEnabled: true,
+        logs: { level: LogLevel.ALL, destination: logGroup, includeExecutionData: true },
       },
       sourceFile: "./src/program.ts",
     });
