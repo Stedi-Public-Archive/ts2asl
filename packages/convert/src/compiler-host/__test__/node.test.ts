794580496737c8d9192ff3498eed6f8a670ad473
Olaf Conijn <olaf_conijn@hotmail.com>
2022-06-01 22:09:17 +0200

    fix: functions resolve to unknown in playground (#115)

---
+++ packages/convert/src/compiler-host/__test__/node.test.ts
@@ -0,0 +1,49 @@
+import { readFileSync } from "fs";
+import ts from "typescript";
+import { convertType } from "../../convert-asllib-to-iasl/helper";
+import { createCompilerHostFromFile, createCompilerHostFromSource } from "../node";
+import { createCompilerHostFromSourceForWeb } from "../web";
+
+const filePath = "./src/compiler-host/__test__/resources/lib-test.ts";
+const source = readFileSync(filePath).toString("utf-8");
+const hostFromFile = createCompilerHostFromFile("./src/compiler-host/__test__/resources/lib-test.ts");
+const hostFromSource = createCompilerHostFromSource(source);
+const hostForWeb = createCompilerHostFromSourceForWeb(source);
+
+
+const getTypeOfStatement = (host : {sourceFile: ts.SourceFile, typeChecker: ts.TypeChecker}, line: number) => {
+  try {
+    const variableStatement = host.sourceFile.statements[line];
+    if (!ts.isVariableStatement(variableStatement)) return ""
+    const decl = variableStatement.declarationList.declarations[0];
+    if (!ts.isVariableDeclaration(decl)) return ""
+    const type = host.typeChecker.getTypeAtLocation(decl.name);
+    const symbol = host.typeChecker.getSymbolAtLocation(decl.name);
+    const iaslType = convertType(type, symbol);
+    return iaslType;
+  } catch(err) {
+    return err;
+  }
+}
+
+describe("when creating host based on string", () => {
+  test("then asl-lib resolves in host from file", () => {
+    const host = hostFromFile;
+    const types = [getTypeOfStatement(host, 2),getTypeOfStatement(host, 3), getTypeOfStatement(host, 4), getTypeOfStatement(host, 5)];
+    expect(types).toEqual(["numeric", "string", "callable-statemachine", "callable-lambda"]);
+  });
+
+  test("then asl-lib resolves in host from source", () => {
+    const host = hostFromSource;
+    expect(host.sourceFile).toBeDefined();
+    const types = [getTypeOfStatement(host, 2),getTypeOfStatement(host, 3), getTypeOfStatement(host, 4), getTypeOfStatement(host, 5)];
+    expect(types).toEqual(["numeric", "string", "callable-statemachine", "callable-lambda"]);
+  });
+
+  test("then asl-lib resolves in host from source for web", () => {
+    const host = hostForWeb;
+    expect(host.sourceFile).toBeDefined();
+    const types = [getTypeOfStatement(host, 2),getTypeOfStatement(host, 3), getTypeOfStatement(host, 4), getTypeOfStatement(host, 5)];
+    expect(types).toEqual(["numeric", "string", "callable-statemachine", "callable-lambda"]);
+  });
+});
--- packages/convert/src/compiler-host/web.ts
+++ packages/convert/src/compiler-host/web.ts
@@ -1,28 +1,51 @@
 
 
-import ts  from "typescript";
+import ts, { getDefaultLibFileName }  from "typescript";
 import * as lib from "../resources/asl-lib"
+
 
 export const createCompilerHostFromSourceForWeb = (source: string) => {
   let compilerOptions = { strict: false, target: ts.ScriptTarget.Latest, allowJs: true, module: ts.ModuleKind.Node12 } as ts.CompilerOptions;
   const contents = source;
   const sourceFile = ts.createSourceFile("ad-hoc.ts", contents, ts.ScriptTarget.ES2015);
-  const host = ts.createCompilerHost(compilerOptions);
   const aslLibSource = ts.createSourceFile("asl-lib.ts", lib.libraryDefinitionAsString, ts.ScriptTarget.Latest)
+  const host: ts.CompilerHost = {
+    getSourceFile: function (fileName: string, languageVersion: ts.ScriptTarget, onError?: (message: string) => void, shouldCreateNewSourceFile?: boolean): ts.SourceFile | undefined {
+      const result = [sourceFile, aslLibSource].find(x => x.fileName === fileName);
 
-  const oldGetSourceFile = host.getSourceFile;
-  host.useCaseSensitiveFileNames = () => true;
-  host.getSourceFile = (filename: string, languageVersion: ts.ScriptTarget) => {
-    const result = [sourceFile, aslLibSource].find(x => x.fileName === filename) ?? oldGetSourceFile(filename, languageVersion);
-    if (result === undefined) {
-      console.log(`${filename} not found`);
+      return result;
+    },
+    resolveModuleNames: (moduleNames: string[]): (ts.ResolvedModule | undefined)[] => {
+      return moduleNames.map(x => (x === "@ts2asl/asl-lib") ? { resolvedFileName: "asl-lib.ts" } : undefined);
+    },
+    getDefaultLibFileName: function (options: ts.CompilerOptions): string {
+      return getDefaultLibFileName(options)
+    },
+    writeFile: function (fileName: string, data: string, writeByteOrderMark: boolean, onError?: (message: string) => void, sourceFiles?: readonly ts.SourceFile[]): void {
+
+    },
+    getCurrentDirectory: function (): string {
+      return "/"
+    },
+    getCanonicalFileName: function (fileName: string): string {
+      return fileName;
+    },
+    useCaseSensitiveFileNames: function (): boolean {
+      return true;
+    },
+    getNewLine: function (): string {
+      return "\n";
+    },
+    fileExists: function (fileName: string): boolean {
+      return [sourceFile, aslLibSource].find(x => x.fileName === fileName) !== undefined;
+    },
+    readFile: function (fileName: string): string | undefined {
+      const file = [sourceFile, aslLibSource].find(x => x.fileName === fileName);
+      return file?.getFullText();
     }
-    return result;
-  };
-  host.resolveModuleNames = (moduleNames: string[]): (ts.ResolvedModule | undefined)[] => {
-    return moduleNames.map(x => (x === "@ts2asl/asl-lib") ? { resolvedFileName: "asl-lib.ts" } : undefined);
   };
   host.getCurrentDirectory = () => "/";
+
   const program = ts.createProgram(["ad-hoc.ts"], compilerOptions, host);
   const typeChecker = program.getTypeChecker();
   return { sourceFile, typeChecker, program };
