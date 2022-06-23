b6064c8e4b488ee4e85f14db7a0ece9c213c24ba
Olaf Conijn <olaf_conijn@hotmail.com>
2022-06-23 21:54:56 +0200

    chore: set up cli project

---
+++ packages/cli/src/commands/compile/index.ts
@@ -0,0 +1,23 @@
+import {Command} from '@oclif/core'
+
+export default class Hello extends Command {
+  static description = 'Compile TypeScript to AWS ASL'
+
+  static examples = [
+    `$ oex hello friend --from oclif
+hello friend from oclif! (./src/commands/hello/index.ts)
+`,
+  ]
+
+  // static flags = {
+  //   from: Flags.string({char: 'f', description: 'Whom is saying hello', required: true}),
+  // }
+
+  //static args = [{name: 'person', description: 'Person to say hello to', required: true}]
+
+  async run(): Promise<void> {
+    const {args, flags} = await this.parse(Hello)
+
+    this.log(`hello ${args.person} from ${flags.from}! (./src/commands/hello/index.ts)`)
+  }
+}
