8d2d249f05d75e6719aa284c05b8306db45738ae
Olaf Conijn <olaf_conijn@hotmail.com>
2022-07-29 00:25:26 +0200

    chore: rename asl-lib-generator to sdk-bundler

--- tools/sdk-bundler/bundle-clients.ts
+++ tools/sdk-bundler/bundle-clients.ts
@@ -1,23 +1,28 @@
 import * as  fs from "fs";
-import * as sdkClients from "./data/aws-sdk-clients.json";
+import * as sdkClients from "./npm/data/aws-sdk-clients.json";
 import * as dtsb from "dts-bundle";
 
-const npmModulesRoot = "./node_modules";
+const npmModulesRoot = "./npm/node_modules";
 const clientNames: Array<string> = [];
 const moduleNames: Array<string> = [];
 
+if (!fs.existsSync("./npm/dts")) {
+  fs.mkdirSync("./npm/dts")
+}
+
 for(const module of Object.values(sdkClients)) {
   const moduleDir = npmModulesRoot + "/" + module.name ;
+  console.log(`bundling: ${module.name}`)
   const distTypesDir = moduleDir + "/dist-types" 
   if (!fs.existsSync(distTypesDir)) continue;
   const contents = fs.readdirSync(distTypesDir);
   const clientFile = contents.filter(file=>/[a-zA-Z0-9]*?Client.d.ts/.test(file));
   if (clientFile.length !== 1) continue;
   const clientName = clientFile[0].substring(0, clientFile[0].length -11)
-  dtsb.bundle({main: distTypesDir + "/" + clientName + ".d.ts", baseDir: "./" , name: module.name, out: "./dts/" + module.name + ".d.ts"});
+  dtsb.bundle({main: distTypesDir + "/" + clientName + ".d.ts", baseDir: "./" , name: module.name, out: "./npm/dts/" + module.name + ".d.ts"});
   clientNames.push(clientName);
   moduleNames.push(module.name)
 }
 
-fs.writeFileSync( "./data/client-names.json", JSON.stringify(clientNames.sort(), null, 2))
-fs.writeFileSync( "./data/module-names.json", JSON.stringify(moduleNames.sort(), null, 2))
+fs.writeFileSync( "./npm/data/client-names.json", JSON.stringify(clientNames.sort(), null, 2))
+fs.writeFileSync( "./npm/data/module-names.json", JSON.stringify(moduleNames.sort(), null, 2))
--- tools/sdk-bundler/create-package-json.ts
+++ tools/sdk-bundler/create-package-json.ts
@@ -1,5 +1,5 @@
 import * as  fs from "fs";
-import * as sdkClients from "./data/aws-sdk-clients.json";
+import * as sdkClients from "./npm/data/aws-sdk-clients.json";
 import * as pjson from "./package.json";
 const dependencies: Record<string, string> = {}
 for(const module of Object.values(sdkClients).map(x=>x.name).filter(x=>!!x)) {
@@ -9,4 +9,4 @@ for(const module of Object.values(sdkClients).map(x=>x.name).filter(x=>!!x)) {
 const copy = {...pjson, dependencies, default: undefined };
 delete copy.default;
 
-fs.writeFileSync( "./package.json", JSON.stringify(copy, null, 2))
+fs.writeFileSync( "./npm/package.json", JSON.stringify(copy, null, 2))
